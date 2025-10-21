e840f62163acd2d5083af1c5bf766919804cf44c
Dana Tal <dana-tal@012.net.il>
2025-10-21 09:49:23 +0300

    adding screenshots

--- spotifyClient/src/components/Welcome.jsx
+++ spotifyClient/src/components/Welcome.jsx
@@ -6,7 +6,7 @@ function Welcome() {
   return (
     <div className="welcome-container">
       <div className="login-container">
-        <h1>Welcome :-)</h1>
+        <h1>Welcome</h1>
         <a href={`${DOMAIN}/auth/login`}>
           <button className="login-button">Login with Spotify</button>
         </a> 
